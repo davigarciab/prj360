ede27a30710f30a126e971df8cc5115fd20ebcaa
davigarciab <83096625+davigarciab@users.noreply.github.com>
2025-02-27 10:05:11 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,56 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img360",
+      "name": "img360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.9028017292157315,
+        "pitch": 0.3005205073460999,
+        "fov": 1.3990458089307722
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9830784239411976,
+          "pitch": 0.13138322502565636,
+          "rotation": 6.283185307179586,
+          "target": "0-img360"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.224216564984273,
+          "pitch": 0.04765609237909629,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
